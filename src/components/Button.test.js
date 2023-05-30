2c84d98b61b4bddfdb21d6b40ae7da3b9900ed24
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-05-30 16:16:59 -0700

    test: adds more tests for button

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -3,7 +3,17 @@ import { shallowMount } from '@vue/test-utils';
 import Button from './Button.vue';
 
 describe('Button.vue', () => {
-  test('renders passed props and displays text', () => {
+  test('renders props with default prop for button type', () => {
+    const displayText = 'This is a test';
+    const shouldDisable = false;
+    const wrapper = shallowMount(Button, {
+      props: { displayText, shouldDisable }
+    });
+
+    expect(wrapper.attributes('type')).toMatch('button');
+  });
+
+  test('renders displayed text', () => {
     const displayText = 'This is a test';
     const shouldDisable = false;
     const wrapper = shallowMount(Button, {
@@ -11,5 +21,15 @@ describe('Button.vue', () => {
     });
 
     expect(wrapper.text()).toMatch('This is a test');
+  });
+
+  test('renders a disabled button', () => {
+    const displayText = 'This is a test';
+    const shouldDisable = true;
+    const wrapper = shallowMount(Button, {
+      props: { displayText, shouldDisable }
+    });
+
+    expect(wrapper.attributes('disabled')).toMatch('');
   });
 });
