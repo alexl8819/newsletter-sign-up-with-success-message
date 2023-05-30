b1d4f06e6521d13d3d8bec2d8b0146fc13ed42f9
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-05-30 14:57:19 -0700

    ui: adds back and properly implements fade out-in transitions, removes unused Transition import, fixes email ref not using .value in computed statement

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup>
-  import { ref, computed,  /*Transition*/ } from 'vue';
+  import { ref, computed } from 'vue';
   import isValidEmail from 'is-valid-email';
 
   import SignUp from './components/SignUp.vue';
   import SuccessConfirmation from './components/SuccessConfirmation.vue';
 
   const email = ref('');
-  const emailConfirmed = computed(() => email && email.value.length && isValidEmail(email.value));
+  const emailConfirmed = computed(() => email.value && email.value.length && isValidEmail(email.value));
 </script>
 
 <template>
   <div class="container">
-    <SuccessConfirmation v-model:email="email" v-if="emailConfirmed" />
-    <SignUp v-model:email="email" :error="email ? !emailConfirmed : false" v-else />
+    <Transition name="fade" mode="out-in">
+      <SuccessConfirmation v-model:email="email" v-if="emailConfirmed" />
+      <SignUp v-model:email="email" :error="email ? !emailConfirmed : false" v-else />
+    </Transition>
   </div>
 </template>
 
@@ -21,17 +23,13 @@
     background-color: var(--custom-white);
   }
 
-  /*
-  .v-enter-active,
-  .v-leave-active {
-    transition: opacity 0.25s ease-out;
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
   }
-
-  .v-enter-from,
-  .v-leave-to {
+  
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
     opacity: 0;
   }
-  */
 
   @media screen and (width >= 1024px) {
     .container {
