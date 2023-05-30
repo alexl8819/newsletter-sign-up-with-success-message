501041048ad3356d15bbe3ec8369795c777710b2
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-05-30 12:50:46 -0700

    test: new test added for SignUp component

--- src/components/SignUp.test.js
+++ src/components/SignUp.test.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 
 import SignUp from './SignUp.vue';
 
-describe('SuccessConfirmation.vue', () => {
+describe('Signup.vue', () => {
   test('renders initial props.email to update and props.error when passed', () => {
     const email = '';
     const error = false;
@@ -10,5 +10,18 @@ describe('SuccessConfirmation.vue', () => {
       props: { email, error }
     });
     expect(wrapper.text()).toMatch(email);
+  });
+
+  test('form emits an update event to parent when value is submitted', async () => {
+    const email = '';
+    const error = false;
+    const wrapper = shallowMount(SignUp, {
+      props: { email, error }
+    });
+    
+    await wrapper.find('input').setValue('test@test.com');
+    await wrapper.find('button').trigger('submit');
+
+    expect(wrapper.emitted()).toHaveProperty('update:email');
   });
 });
